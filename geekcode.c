27593a9f19139687c097084a1cba1305f67a6531
Charlie Shepherd <masterdriverz@gentoo.org>
2007-09-22 21:11:13 +0200

    Give answer structs names instead of using the preprocesser (necessary since they are now named gc_*).

--- geekcode.c
+++ geekcode.c
@@ -35,57 +35,57 @@
 #include "gc_type.h"
 #include "gc_getanswer.h"
 
-#define HIDDEN(name)			{-1, 0, #name, name, 0},
-#define INDEPENDANT(name)		{-1, 1, #name, name, 0},
-#define DEPENDANT(name)			{-1, 1, #name, name, 1},
+#define HIDDEN(ident, name)		{-1, 0, name, ident, 0},
+#define INDEPENDANT(ident, name)	{-1, 1, name, ident, 0},
+#define DEPENDANT(ident, name)		{-1, 1, name, ident, 1},
 #define LINE_END			{0,  0, NULL,  NULL, 0}
 
 struct stuff2 line1[] = {
-	INDEPENDANT(gc_type)
-	INDEPENDANT(gc_dress)
-	INDEPENDANT(gc_height)
-	INDEPENDANT(gc_weight)
-	INDEPENDANT(gc_age)
-	INDEPENDANT(gc_computers)
-	HIDDEN(gc_unix_type)
-	DEPENDANT(gc_unix)
-	INDEPENDANT(gc_perl)
-	INDEPENDANT(gc_linux)
-	INDEPENDANT(gc_emacs)
-	INDEPENDANT(gc_www)
-	INDEPENDANT(gc_usenet)
-	INDEPENDANT(gc_oracle)
-	INDEPENDANT(gc_kibo)
-	INDEPENDANT(gc_windows)
+	INDEPENDANT(gc_type, "type")
+	INDEPENDANT(gc_dress, "dress")
+	INDEPENDANT(gc_height, "height")
+	INDEPENDANT(gc_weight, "weight")
+	INDEPENDANT(gc_age, "age")
+	INDEPENDANT(gc_computers, "computers")
+	HIDDEN(gc_unix_type, "unix type")
+	DEPENDANT(gc_unix, "unix")
+	INDEPENDANT(gc_perl, "perl")
+	INDEPENDANT(gc_linux, "linux")
+	INDEPENDANT(gc_emacs, "emacs")
+	INDEPENDANT(gc_www, "WWW")
+	INDEPENDANT(gc_usenet, "usenet")
+	INDEPENDANT(gc_oracle, "oracle")
+	INDEPENDANT(gc_kibo, "kibo")
+	INDEPENDANT(gc_windows, "windows")
 	LINE_END
 };
 
 struct stuff2 line2[] = {
-	INDEPENDANT(gc_os2)
-	INDEPENDANT(gc_mac)
-	INDEPENDANT(gc_vms)
-	INDEPENDANT(gc_social)
-	INDEPENDANT(gc_economic)
-	INDEPENDANT(gc_cypher)
-	INDEPENDANT(gc_pgp)
-	INDEPENDANT(gc_startrek)
-	INDEPENDANT(gc_babylon5)
-	INDEPENDANT(gc_xfiles)
-	INDEPENDANT(gc_rp)
-	INDEPENDANT(gc_television)
-	INDEPENDANT(gc_books)
-	INDEPENDANT(gc_dilbert)
-	INDEPENDANT(gc_doom)
+	INDEPENDANT(gc_os2, "OS2")
+	INDEPENDANT(gc_mac, "mac")
+	INDEPENDANT(gc_vms, "vms")
+	INDEPENDANT(gc_social, "social")
+	INDEPENDANT(gc_economic, "economic")
+	INDEPENDANT(gc_cypher, "cypher")
+	INDEPENDANT(gc_pgp, "PGP")
+	INDEPENDANT(gc_startrek, "startrek")
+	INDEPENDANT(gc_babylon5, "bablylon5")
+	INDEPENDANT(gc_xfiles, "XFiles")
+	INDEPENDANT(gc_rp, "roleplaying")
+	INDEPENDANT(gc_television, "television")
+	INDEPENDANT(gc_books, "books")
+	INDEPENDANT(gc_dilbert, "dilbert")
+	INDEPENDANT(gc_doom, "doom")
 	LINE_END
 };
 
 struct stuff2 line3[] = {
-	INDEPENDANT(gc_geekcode)
-	INDEPENDANT(gc_education)
-	INDEPENDANT(gc_housing)
-	INDEPENDANT(gc_relationships)
-	HIDDEN(gc_gender)
-	DEPENDANT(gc_sex)
+	INDEPENDANT(gc_geekcode, "geekcode")
+	INDEPENDANT(gc_education, "education")
+	INDEPENDANT(gc_housing, "housing")
+	INDEPENDANT(gc_relationships, "relationships")
+	HIDDEN(gc_gender, "gender")
+	DEPENDANT(gc_sex, "sex")
 	LINE_END
 };
 
