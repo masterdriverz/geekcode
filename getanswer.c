fd31b3ce87432fcbefb563493ca40a7f3c1d770e
Charlie Shepherd <masterdriverz@gmail.com>
2008-04-11 18:32:57 +0100

    Don't right-align page count
    Remove stupid code that attempts to right align the page count and just put it
    on the left.

--- getanswer.c
+++ getanswer.c
@@ -19,33 +19,6 @@
 	exit(1);			\
 } while (0);
 
-static char *make_spaces(unsigned int amount)
-{
-	static char s[67];
-	static int prev=-1;
-
-	if (prev == -1) {
-		memset(s, ' ', amount);
-		s[66] = '\0';
-	} else if (amount == prev) {
-		return s;
-	} else {
-		s[prev] = ' ';
-	}
-#ifdef DEBUG
-	printf("\nprev: %d\namount: %d\nspaces: '%s'\n",
-		prev, amount, s);
-#endif
-
-	prev = amount;
-	s[amount] = '\0';
-
-#ifdef DEBUG
-	printf("len: %d\n", strlen(s));
-#endif
-	return s;
-}
-
 /*
  * Empties keyboard input buffer until it reaches a newline.
  * Returns 1 on EOF, 0 otherwise.
@@ -64,10 +37,9 @@ int getanswer(const char *name, int page_num,
 		const char *additional)
 {
 	int selection, i;
-	char *spaces=make_spaces(66-strlen(name));
 
 	do {
-		printf("%s%sPage %2i of %i\n", name, spaces, page_num, PAGES);
+		printf("\nPage %2i of %i: %s\n", page_num, PAGES, name);
 		puts("===============================================================================");
 		for (i=0; objects[i].comment; i++) {
 			const char *alias = objects[i].alias;
