384e041d3de1e9b7988cab7ef1137065a3b9b05f
Charlie Shepherd <masterdriverz@gmail.com>
2008-04-12 17:10:05 +0100

    First attempt to use getline and strtol instead of scanf

--- getanswer.c
+++ getanswer.c
@@ -5,7 +5,9 @@
  * License: GPL2
  */
 
+#define _GNU_SOURCE
 #include <errno.h>
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -19,57 +21,88 @@
 	exit(1);			\
 } while (0);
 
-/*
- * Empties keyboard input buffer until it reaches a newline.
- * Returns 1 on EOF, 0 otherwise.
- */
-static int clear_kb(void)
+/* Prints a single page (answer). Returns the number of elems printed */
+static unsigned int print_page(const char *name, int page_num,
+		const struct elem *objects,
+		const char *additional)
 {
-	int c;
-	while ((c = getchar()) != '\n')
-		if (c == EOF)
-			return 1;
-	return 0;
+	unsigned int i;
+	printf("\nPage %2i of %i: %s\n", page_num, PAGES, name);
+	puts("===============================================================================");
+	for (i = 0; objects[i].comment; i++) {
+		const char *alias = objects[i].alias;
+		if (additional) {
+			/* The %c is replaced by a char and a NULL,
+			 * hence no +1 */
+			char *s = malloc(strlen(objects[i].alias));
+			if (!s) {
+				perror(NULL);
+				exit(-1);
+			}
+			sprintf(s, objects[i].alias, *additional);
+			alias = s;
+		}
+		printf("%2d %-5s %s\n", i+1, alias, objects[i].comment);
+		if (additional)
+			free((void *)alias);
+	}
+	return i;
 }
 
 int getanswer(const char *name, int page_num,
 		const struct elem *objects,
 		const char *additional)
 {
-	int selection, i;
+	static char *p = NULL;
+	static size_t n;
+	int max;
+	char *s;
+	char *endptr;
+	long val;
 
-	do {
-		printf("\nPage %2i of %i: %s\n", page_num, PAGES, name);
-		puts("===============================================================================");
-		for (i=0; objects[i].comment; i++) {
-			const char *alias = objects[i].alias;
-			if (additional) {
-				/* The %c is replaced by a char and a NULL,
-				 * hence no +1 */
-				char *s = malloc(strlen(objects[i].alias));
-				if (!s) {
-					perror(NULL);
-					exit(-1);
-				}
-				sprintf(s, objects[i].alias, *additional);
-				alias = s;
-			}
-			printf("%2d %-5s %s\n", i+1, alias, objects[i].comment);
-			if (additional)
-				free((void *)alias);
-		}
-		printf("\nEnter your %s code number here [0 to quit]: ", name);
-		if (scanf("%d", &selection) == EOF)
+print:
+	max = print_page(name, page_num, objects, additional);
+
+	putc('\n', stdout);
+foo:
+	/*
+	if (selection != -1)
+		fputs(stdout, "Invalid option: ");
+		*/
+	printf("Enter your %s code number here [q to quit, 0 to repeat]: ", name);
+	if (getline(&p, &n, stdin) == -1) {
+		if (feof(stdin))
 			eof_error();
+		perror(NULL);
+		exit(1);
+	}
+	s = p;
+	errno = 0;
+	val = strtol(s, &endptr, 10);
 
-		if (clear_kb())
-			eof_error();
-	} while (selection < 0 || selection > i);
+	/* Check for various possible errors */
 
-	if (selection == 0)
-		exit(0);
-	else
-		return selection;
+	if ((errno == ERANGE && (val == LONG_MAX || val == LONG_MIN))
+		|| (errno != 0 && val == 0)) {
+		perror("strtol");
+		exit(1);
+	}
+
+	if (endptr == s) {
+		fprintf(stderr, "No digits were found\n");
+		exit(1);
+	}
+
+		while (*s == ' ')
+			s++;
+		if (*s == 'q' || *s == 'Q')
+			exit(0);
+	if (val > max)
+		goto foo;
+	if (val == 0)
+		goto print;
+
+	return (int)val;
 }
 
 const struct elem *getcontent(const struct answer *obj)
